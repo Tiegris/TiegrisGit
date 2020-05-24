24ba54be9d7b5604a4cc2b3cf8846511c273e5e9
tieger <tieger@sch.bme.hu>
2020-05-24 02:44:02 +0200

    removed unnecesary usings

--- TiegrisUtil/TiegrisUtil/Frameworks/IConsoleProgramArgsValidator.cs
+++ TiegrisUtil/TiegrisUtil/Frameworks/IConsoleProgramArgsValidator.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace TiegrisUtil.Frameworks
+﻿namespace TiegrisUtil.Frameworks
 {
     /// <summary>
     /// Common interface for argument validator classes.
